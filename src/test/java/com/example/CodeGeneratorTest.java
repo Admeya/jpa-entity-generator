5c7efbe43d64810f4adb1240ff04f9bfe8ac1f2b
Irina Bykova <bykovaie@sovcombank.ru>
2024-04-03 09:18:47 +0300

    to java11

--- src/test/java/com/example/CodeGeneratorTest.java
+++ src/test/java/com/example/CodeGeneratorTest.java
@@ -23,7 +23,7 @@ public class CodeGeneratorTest {
     public void _01_generateAll_TableScanMode_Is_Default() throws Exception {
         CodeGeneratorConfig config = CodeGeneratorConfig.load("entityGenConfig.yml");
         config.setJpa1SupportRequired(true);
-        config.setOutputDirectory("src/test/java");
+        config.setOutputDirectory("src/test/java/goal");
         CodeGenerator.generateAll(config, true);
         CodeGenerator.generateAll(config, false);
     }
